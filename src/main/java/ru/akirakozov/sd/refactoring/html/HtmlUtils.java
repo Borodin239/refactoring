61a191aea33fbb2d5c4efaa689765d38ca68930d
Borodin239 <j.borodin1@mail.ru>
2022-11-05 16:20:47 +0300

    Bug fix: replace StringBuilder with String

--- src/main/java/ru/akirakozov/sd/refactoring/html/HtmlUtils.java
+++ src/main/java/ru/akirakozov/sd/refactoring/html/HtmlUtils.java
@@ -5,15 +5,15 @@ import ru.akirakozov.sd.refactoring.data.Product;
 import java.util.List;
 
 public class HtmlUtils {
-    private static String addHeaders(StringBuilder body, String header) {
+    private static String addHeaders(String body, String header) {
         return "<html><body>\n" +
             header +
-            body.toString() +
+            body +
             "</body></html>";
     }
 
     public static String getStatisticHTML(Integer res, String header) {
-        return addHeaders(new StringBuilder(res), header);
+        return addHeaders(res + "\n", header);
     }
 
     public static String getProductsHTML(List<Product> products, String header) {
@@ -21,7 +21,7 @@ public class HtmlUtils {
         for (Product product: products) {
             res.append(addProduct(product));
         }
-        return addHeaders(res, header);
+        return addHeaders(res.toString(), header);
     }
 
     private static StringBuilder addProduct(Product product) {
